fe6e25985ab9cf021a7835dee0a3c575c01ff700
Volodymyr Pestov <pestov.v@yahoo.com>
2021-02-26 23:11:12 +0200

    client: redirect from game without user

--- client/src/Game/GameContainer.tsx
+++ client/src/Game/GameContainer.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Game from "./Game";
+import { useStateValue } from "../context/StateProvider";
+import { Redirect } from "react-router-dom";
 
 class GameContainer extends Component<{}, {}> {
   constructor(props: {}) {
@@ -27,4 +29,12 @@ class GameContainer extends Component<{}, {}> {
   }
 }
 
-export default GameContainer;
+const GameContainerWrapper = () => {
+  const {
+    state: { user },
+  } = useStateValue();
+  if (!user.id) return <Redirect to="/" />;
+  return <GameContainer />;
+};
+
+export default GameContainerWrapper;
